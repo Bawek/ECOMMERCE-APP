94485d38f46721aaec4b55fd72fdc134bb26d118
Bawek <bawekeasres@gmail.com>
2024-11-09 15:56:32 +0300

    product page

--- frontends/src/pages/Collection.jsx
+++ frontends/src/pages/Collection.jsx
@@ -6,13 +6,23 @@ const Collection = () => {
     <div className='flex flex-col sm:flex-row gap-4 '>
     <div className='min-w-60' >
 
-    <div>
+    <div className='cursor-pointer' onClick={()=>setShow(!show)}>
     <p className='text-2xl font-medium'> FILTERS</p>
+    </div>
 
+    <div className={`flex p-6 flex-col border ${show} ? "" : hidden`}>
+    <p className='font-medium text-xl '>CATEGORIES</p>
+    <div className='flex gap-2'>
+    <input type="checkbox" name="" id="" value="Men"/>Men</div>
+    <div className='flex gap-2'>
+    <input type="checkbox" name="" id="" value="Women"/>Women</div>  <div className='flex gap-2'>
+    <input type="checkbox" name="" id="" value="Kids"/>Kids</div>
     </div>
     
     </div>
-    
+    <div>
+    Collection Page
+    </div>
     
     </div>
   )
